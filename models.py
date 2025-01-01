2b02544dbe2709bb22f767ff00c62b13a71ecf4d
denismnjima <denismnjima@gmail.com>
2025-01-01 18:30:29 +0300

    adds models for db tables

---
+++ models.py
@@ -0,0 +1,40 @@
+from database import Base
+from sqlalchemy import Column,Integer,String,Text,Boolean,ForeignKey
+from sqlalchemy_utils.type import ChoiceType
+from sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer,primary_key=True)
+    username = Column(String(50),unique=True)
+    email = Column(String(50),unique=True)
+    password = Column(Text)
+    is_active = Column(Boolean, default=False)
+    is_staff = Column(Boolean, default=False)
+    orders = relationship('Order', back_populates='user')
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+class Order(Base):
+
+    ORDER_STATUS = (
+        ('PENDING', 'pending'),
+        ('IN_TRANSIT', 'in-transit'),
+        ('DELIVERED', 'delivered')
+    )
+    PIZZA_SIZE = (
+        ('SMALL','small'),
+        ('MEDIUM','medium'),
+        ('LARGE', "large")
+    )
+    __tablename__ = 'orders'
+
+    id = Column(Integer, primary_key=True)
+    quantity = Column(Integer,nullable=True)
+    order_status = Column(ChoiceType(choices= ORDER_STATUS), default='PENDING')
+    pizza_size = Column(ChoiceType(choices = PIZZA_SIZE))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='orders')
