42c141cf4534634332403674b3ac53798a7acce2
denismnjima <denismnjima@gmail.com>
2025-01-01 18:09:38 +0300

    adds pass as env variable

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
 
-engine = create_engine('postresql://postgres:[pass]@localhost/pizza_delivery', echo=True)
+load_dotenv()
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+print(DB_PASSWORD)
+
+engine = create_engine(f'postgresql://postgres:{DB_PASSWORD}@localhost/pizza_delivery', echo=True)
 
 Base = declarative_base()
 Session = sessionmaker()
